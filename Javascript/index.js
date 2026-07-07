51ffad647bd6337f3a816a6b3cf0ad2618ea9065
Dixo_n <chidubemdickson123@gail.com>
2026-07-07 12:16:32 +0100

    "Commit message"

---
+++ Javascript/index.js
@@ -0,0 +1,5 @@
+console.log("Hello, welcome to the introduction!");
+
+console.log("I'm learning JavaScript");
+
+console.log("Hello" + " and " + "Goodbye"); // No error: strings are defined
---
+++ Javascript/intro.js
@@ -0,0 +1,68 @@
+// This is a simple JavaScript program that prints "Hello, World!" to the console.
+console.log("Hello, World!");
+
+console.log("Hello, welcome to the introduction!"); 
+// let president = "George Washington";
+// let message =   "Hello, welcome to the introduction!"
+// console.log(message);
+// message ="Nice weather!";
+// console.log(message);
+
+// This is a simple JavaScript program that prints "Hello, World!" to the console.
+console.log("Hello, World!");
+// declare a var that can change
+// let president = "George washington";
+// let message = "Hello World!";
+
+// how to name a var
+//  the code below is a valid way to name a var
+// message_2,  _message3 but you cant start with a number and you cant use a symbol or hyohen to name a var
+
+// camelCase is a common way to name a var in JavaScript. For example: myVariableName
+
+// let accountNumber = "1234567890";
+// let myAwesomeVariable;
+// let my_variable_awesome;
+
+// Const is a variable that cannot be changed
+if (true) {
+    const PI = 3.142;
+}
+
+//let area = PI * R * R;
+
+//algorithm to calculate the area of a circle
+// VAR
+//   Radius, AREA:FLOAT
+//   PI:FLOAT
+//   BEGIN
+//   AREA = PI * R * R
+//   PRINT (AREA)
+// END
+
+// DIFF var, let, const
+// var is function scoped, let and const are block scoped
+// var can be re-declared and updated, let can be updated but not re-declared, const cannot be updated or re-declared
+
+// if (true) {
+//     var name = "Ejiekpe Chidubem";
+//     let phone_Number = "08053322926";
+//     const email = "ejiekpe.chidubemdikson123@gmail.com";
+    
+// }
+
+//  if (true) {
+//       let name = "John";
+// }
+// cnsole.log(name); // Error: name is not defined
+
+let F_name = "Ejiekpe Chidubem";
+let topic = "JavaScript today";
+console.log("The message is: " + F_name + " is learning " + topic); // No error: strings are defined
+
+// template literals
+let senator_1 = "Abiodun";
+let topic_1 = "Baking";
+
+console.log(`${senator_1} is learning ${topic_1} today`);
+
