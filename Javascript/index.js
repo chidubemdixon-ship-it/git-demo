e28f45c8fc272596f594fc6951a9dd46d465bc67
Dixo_n <chidubemdickson123@gail.com>
2026-07-08 10:22:53 +0100

    "Commit Message"

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,3 +3,71 @@ console.log("Hello, welcome to the introduction!");
 console.log("I'm learning JavaScript");
 
 console.log("Hello" + " and " + "Goodbye"); // No error: strings are defined
+
+// Exercise
+
+// Create an array named colors that include the 'red', 'green, and 'blue' colors.
+
+// First, add a 'black' color after the last index of the array. Then print the array.
+
+// Next, remove the value 'red' and swap the position of 'green' and 'blue'. Print the array.
+
+// Finally, add the color 'yellow' on the first index of the array, then print the array.
+
+let colors = ['red', 'green', 'blue'];
+
+colors.push('black');
+console.log(colors);
+
+colors.splice(colors.indexOf('red'), 1);
+let greenIndex = colors.indexOf('green');
+let blueIndex = colors.indexOf('blue');
+[colors[greenIndex], colors[blueIndex]] = [colors[blueIndex], colors[greenIndex]];
+console.log(colors);
+
+colors.unshift('yellow');
+console.log(colors);
+
+
+
+// control flows (cnditionals) in javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if statement
+let age = 18;
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+}
--- Javascript/intro.js
+++ Javascript/intro.js
@@ -66,3 +66,130 @@ let topic_1 = "Baking";
 
 console.log(`${senator_1} is learning ${topic_1} today`);
 
+let score = "10" + "30";
+console.log(score);
+
+// Boolean
+let on = true;
+let off = false;
+
+// undefined
+// null 
+// 0
+// let perimeter = null;
+// let area = 0;
+// console.log(area);
+
+let n1 = 1;
+let n2= 2;
+
+// console.log(n1 + n2);
+
+let f = 1.2;
+let z = 2.35;
+
+// console.log(f + z);
+// type coercion
+// convert number from string to integer
+let t = "7";
+let v = 5;
+let y;
+// Convert t to integer
+t = Number(t);
+y = t + v;
+// console.log(typeof y); 
+// console.log(y); // 12
+
+1 + "1" // "11"
+// let a = [1 ,2] + "1"
+// console.log(a); // "1,21"
+// console.log(typeof(a)); // "1,21"
+// true + "1" // "true1"
+
+
+var i = { a: 1 } + "1" // 1
+var j= "1" + { a: 1 } // "1[object Object]"
+var k= true + { a: 1 } // "true[object Object]"
+var l= { a: 1 } + 1 // 1
+
+// console.log(i); // 
+// console.log(j); // 
+// console.log(k);
+// console.log(l); //"
+
+true + 1 // 1+1 = 2
+false + 1 // 0+1 = 1
+
+// arithmetic operators
+// assigment operators
+// comparison operators
+
+// console.log(9 == 9);
+// console.log(9 != 20);
+// console.log(2 > 10);
+// console.log(2 < 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10)
+
+// console.log("ABC" == "ABC");
+// console.log("ABC" == "abc");
+console.log("9" == 9);
+console.log("9" === 9); 
+
+
+// Assingnment Read about logical operators
+// logical operators
+
+// Arrays
+let bird = [];
+let birds = ['Owl', 'Eagle', 'Parrot', 'Falcon'];
+let mixedArray = ['Bird', true, 10, 5.17]
+
+// console.log(birds[0]);
+// console.log(birds[3]);
+
+// console.log(mixedArray[3]);
+console.log(birds);
+
+// special methods for array manipulation
+birds[2] = "Underbird";
+// console.log(birds)
+
+// push() add element to the end of an array
+bird.push('Turkey','Sparrow');
+bird.push('Pigeon');
+console.log(bird);
+
+// console.log(bird[1])
+// console.log(mixedArray)
+
+// pop to remove the last element of an array
+bird.pop();
+console.log(bird);
+
+// unshift() add element to the beginning of an array
+let fishes = ['Salmon', 'Goldfish', 'Tuna'];
+
+fishes.unshift('Sardine');
+
+console.log(fishes);
+
+// to remove in the beginning of an array we use shift()
+fishes.shift();
+
+console.log(fishes);
+
+
+let pos = fishes.indexOf('Tuna');
+
+console.log(pos)
+
+// Exercise
+// Create an array named colors that include the 'red', 'green, and 'blue' colors.
+
+// First, add a 'black' color after the last index of the array. Then print the array.
+
+// Next, remove the value 'red' and swap the position of 'green' and 'blue'. Print the array.
+
+// Finally, add the color 'yellow' on the first index of the array, then print the array.
