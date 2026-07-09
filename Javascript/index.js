4ea246cee6858c3790497211de95912f223656f7
Dixo_n <chidubemdickson123@gail.com>
2026-07-09 12:38:51 +0100

    Coommit Message: Added switch case example in index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,8 +1,8 @@
-console.log("Hello, welcome to the introduction!");
+// console.log("Hello, welcome to the introduction!");
 
-console.log("I'm learning JavaScript");
+// console.log("I'm learning JavaScript");
 
-console.log("Hello" + " and " + "Goodbye"); // No error: strings are defined
+// console.log("Hello" + " and " + "Goodbye"); // No error: strings are defined
 
 // Exercise
 
@@ -14,60 +14,97 @@ console.log("Hello" + " and " + "Goodbye"); // No error: strings are defined
 
 // Finally, add the color 'yellow' on the first index of the array, then print the array.
 
-let colors = ['red', 'green', 'blue'];
+// let colors = ['red', 'green', 'blue'];
 
-colors.push('black');
-console.log(colors);
+// colors.push('black');
+// console.log(colors);
 
-colors.splice(colors.indexOf('red'), 1);
-let greenIndex = colors.indexOf('green');
-let blueIndex = colors.indexOf('blue');
-[colors[greenIndex], colors[blueIndex]] = [colors[blueIndex], colors[greenIndex]];
-console.log(colors);
+// colors.splice(colors.indexOf('red'), 1);
+// let greenIndex = colors.indexOf('green');
+// let blueIndex = colors.indexOf('blue');
+// [colors[greenIndex], colors[blueIndex]] = [colors[blueIndex], colors[greenIndex]];
+// console.log(colors);
 
-colors.unshift('yellow');
-console.log(colors);
+// colors.unshift('yellow');
+// console.log(colors);
 
 
 
 // control flows (cnditionals) in javascript
 
+// Two types of control flow
+// 1. Conditional statements (if, else if, else)
+// 2. Loop statements (for, while, do-while)
 
+// Coditional => if else andswitch case statement
+// if (condition) {
+    // execute this code if the condition is true
+    // }
+//  example
+//   Suppose you want to go on a Vacation that requires 5000 dollars
+//  Print "You have the money for this trip. let's go!" 
+//  Balance = 7000
 
+// let balance = 7000;
+// if (balance >= 5000) {
+//     console.log("You have the money for this trip. let's go!");
+// } else {
+//     console.log("You don't have enough money for this trip.");
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if statement
-let age = 18;
-if (age >= 18) {
-    console.log("You are eligible to vote.");
+let balance = 4999;
+if (balance >= 8000) {
+    console.log("You have the money for this trip. let's go!");
+} else if (balance >= 5000) {
+    console.log("You dey manage");
+} else if (balance > 3000) {
+    console.log("You only have enough money for a staycation");
+} else {
+    console.log("Nobody poor pass you!");
 }
+
+console.log("The end!");
+
+// switch case
+
+let age = 15;
+// switch example
+switch (age) {
+    case 10:
+        console.log("Age is 10");
+        break;
+    case 20:
+        console.log("Age is 20");
+        break;
+    default:
+        console.log("Age is neither 10 nor 20");
+}
+// another switch example
+switch (age) {
+    case 10 + 10:
+        console.log("Age is twenty");
+        break;
+}
+
+    switch (0) {
+        case 0:
+            console.log("Value is one");
+            break;
+        case 1:
+            console.log("Value is Zero");
+            break;
+        default:
+            console.log("No matching case");
+    }
+
+    // || or &&     
+
+    let myFood = "Banana";
+    if (myFood === "Banana" || myFood === "Apple") {
+        console.log("Eat fruits everday to keep you healthy");
+    }
+
+    if (myFood === "Chocolate Cake") {
+        console.log("Eat Fruits everyday to keep you healthy");
+    }
+    
